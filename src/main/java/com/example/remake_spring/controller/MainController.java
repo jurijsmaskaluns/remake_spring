97aa85fb183d8d9ebceffa2df8b291fb1d1890ef
JMJ <jurijsmaskaluns@gmail.com>
2019-09-29 22:02:42 +0300

    registration fail 2

--- src/main/java/com/example/remake_spring/controller/MainController.java
+++ src/main/java/com/example/remake_spring/controller/MainController.java
@@ -46,7 +46,7 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
+    @PostMapping("/filter")
     public String filter(@RequestParam String filter, Map<String,Object> model){
         Iterable<ProductEntity> pE;
 
